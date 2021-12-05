a4411712709d02569eafe7cfba62bfe6be32b4e1
mohammad <aliakbariiimohammad@gmail.com>
2021-12-05 01:31:12 -0800

    make login template

--- markito/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import login
-
-app_name = 'markito'
-urlpatterns = [
-    path('', login, name="login")
-]
--- markito/views.py
+++ markito/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
-def login(request):
-    return render(request,"markito/login.html")
