a0bf6f3523be3dbc057440a92b621e217bdec488
mohammad <aliakbariiimohammad@gmail.com>
2021-12-05 03:03:48 -0800

    create base html and login customer

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
 
+
+from django.contrib.auth import authenticate,login as auth_login,logout
 # Create your views here.
 
 
 def login(request):
-    return render(request,'accounts/login.html')
+    message = ''
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            auth_login(request,user)
+            return render(request,'markito/home.html')
+        else:
+            message = 'your email or password is wrong!!!'
+
+
+
+    return render(request,'accounts/login.html',context={
+        'message':message,
+    })
+
+
+
+
