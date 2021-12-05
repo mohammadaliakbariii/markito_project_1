bbc0f3f453fa61953d84d8d935d22b17d22599c5
mohammad <aliakbariiimohammad@gmail.com>
2021-12-05 05:45:53 -0800

    more correct in register template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login,log_out
+from .views import login,log_out,register
 
 app_name = 'accounts'
 urlpatterns = [
     path('', login, name="login"),
     path('log_out/',log_out,name='log_out'),
+    path('register/',register,name="register")
 ]
