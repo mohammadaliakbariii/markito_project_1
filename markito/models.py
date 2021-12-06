102419b81d287a5d1fc745ac4fb4e1467c40b0f2
mohammad <aliakbariiimohammad@gmail.com>
2021-12-06 05:48:43 -0800

    create channel model for markito app

--- markito/models.py
+++ markito/models.py
@@ -5,6 +5,19 @@ from accounts.models import CustomUser
 # Create your models here.
 
 
+class Channel(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
 class Categories(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
