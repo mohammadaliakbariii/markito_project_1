fbfc1a8a58016fbe470a9da2bf3baf3ff074a35c
mohammad <aliakbariiimohammad@gmail.com>
2021-12-06 04:29:42 -0800

    add dashboard html and their view function

--- markito/views.py
+++ markito/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'markito/home.html')
+
+
+
+def dashboard(request):
+    return render(request,'markito/dashboard.html')
