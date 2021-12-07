28a8af00e6c11730f9633d49f387d688155ff41d
mohammad <aliakbariiimohammad@gmail.com>
2021-12-07 15:28:42 +0330

    delet query of store from product view and reformated that file

--- markito/views.py
+++ markito/views.py
@@ -1,32 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Products,Store
+from .models import Products, Store
 from django.core.paginator import Paginator
+
+
 # Create your views here.
 
 def home(request):
-    return render(request,'markito/home.html')
+    return render(request, 'markito/home.html')
+
 
 @login_required
 def products(request):
-    store = Store.objects.filter(user=request.user)
     products = Products.objects.filter(store__user=request.user)
     p = Paginator(products, 10)
     page = request.GET.get('page')
     products_list = p.get_page(page)
     context = {
-        "list_products":products_list,
+        "list_products": products_list,
 
     }
-    return render(request,'markito/products.html',context)
-
+    return render(request, 'markito/products.html', context)
 
 
 def dashboard(request):
-    return render(request,'markito/dashboard.html')
-
-
-
-
-
-
+    return render(request, 'markito/dashboard.html')
